afaa273bce1e91923e36d1e78e38b2b9f2d49af1
Robert Ball <robert.ball.1989@gmail.com>
2017-04-02 17:48:52 +0100

    Created test for 'shopper' class, which will contain the basket. Created test for getName().

---
+++ Shopping_Basket/app/src/test/java/com/codeclan/shopping_basket/ShopperTest.java
@@ -0,0 +1,29 @@
+package com.codeclan.shopping_basket;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.*;
+
+/**
+ * Created by user on 02/04/2017.
+ */
+
+public class ShopperTest {
+
+    Product pear;
+    Product earphones;
+    Shopper shopper;
+
+    @Before
+    public void before(){
+        pear = new Product("pear", ItemType.FRUIT, 0.5);
+        earphones = new Product("earphones", ItemType.ELECTRICAL, 30);
+        shopper = new Shopper("Kevin");
+    }
+
+    @Test
+    public void testgetName(){
+        assertEquals("Kevin",getName());
+    }
+}
