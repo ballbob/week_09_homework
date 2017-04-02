148293796906d0cac052053b90d06e4e5ce8134d
Robert Ball <robert.ball.1989@gmail.com>
2017-04-02 18:17:53 +0100

    getName function works for cashier. These name functions work as an initial test that the object can be interacted with at all.

---
+++ Shopping_Basket/app/src/main/java/com/codeclan/shopping_basket/Cashier.java
@@ -0,0 +1,27 @@
+package com.codeclan.shopping_basket;
+
+import java.util.ArrayList;
+
+/**
+ * Created by user on 02/04/2017.
+ */
+
+public class Cashier {
+
+    private Product item;
+    private String name;
+    private ArrayList<Product> items;
+    private ItemType discountedType;
+    private int offerThreshold;
+
+    public Cashier(String name){
+        this.name = name;
+        this.items = new ArrayList<Product>();
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+
+}
