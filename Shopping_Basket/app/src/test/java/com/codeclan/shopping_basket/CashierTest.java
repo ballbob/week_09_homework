73ea045e29b02827fd6ffe9bab1f348ebc05421b
Robert Ball <robert.ball.1989@gmail.com>
2017-04-02 18:10:30 +0100

    Created cashier test. The cashier will handle discounts and pricing for the items involved.

---
+++ Shopping_Basket/app/src/test/java/com/codeclan/shopping_basket/CashierTest.java
@@ -0,0 +1,34 @@
+package com.codeclan.shopping_basket;
+
+import org.junit.*;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by user on 02/04/2017.
+ */
+
+public class CashierTest {
+
+    Product pear;
+    Product mango;
+    Product earphones;
+    Shopper shopper;
+    Shopper disloyalShopper;
+    Cashier cashier;
+
+    @Before
+    public void before(){
+        pear = new Product("pear", ItemType.FRUIT, 0.5);
+        mango = new Product("big mango",ItemType.FRUIT,1);
+        earphones = new Product("earphones", ItemType.ELECTRICAL, 30);
+        shopper = new Shopper("Kevin",true);
+        disloyalShopper = new Shopper("Judas",false);
+        cashier = new Cashier("Etienne");
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Etienne",cashier.getName());
+    }
+}
